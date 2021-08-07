58a4de3f49ad08b0644a5b972c1e606cdfc7e7ea
ibra <muhammadibrahimhisham@gmail.com>
2021-08-08 00:58:39 +0500

    implemented comments, summaries, bug fixes

--- Ibralogue/Examples/ExampleScripts/DialogueInput.cs
+++ Ibralogue/Examples/ExampleScripts/DialogueInput.cs
@@ -6,8 +6,8 @@ namespace Ibralogue.Examples
     {
         private void Update()
         {
-            //Preferably, I would have used the new Input System for this, but I didn't want to clutter the examples any more than they 
-            //needed to be.
+            //Preferably, I would have used the new Input System for this, but I didn't want to clutter the examples with
+            //dependencies any more than they needed to be.
             if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
                 DialogueManager.Instance.DisplayNextLine();
             if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Space))
--- Ibralogue/Scripts/DialogueManager.cs
+++ Ibralogue/Scripts/DialogueManager.cs
@@ -10,21 +10,21 @@ namespace Ibralogue
     public class DialogueManager : MonoBehaviour
     {
         public static DialogueManager Instance { get; private set; }
-
-        private string[] _currentDialogueLines;
-        
-        private List<Dialogue> _parsedDialogues;
-
         public static Dictionary<string, string> GlobalVariables = new Dictionary<string, string>();
 
+        private string[] _currentDialogueLines;
+        private List<Dialogue> _parsedDialogues;
+        
         private int _currentDialogueIndex;
         private int _currentSentenceIndex;
+        private bool _linePlaying;
 
         [SerializeField] private TextMeshProUGUI sentenceText;
         [SerializeField] private TextMeshProUGUI nameText;
         [SerializeField] private Image speakerPortrait;
         
-        
+
+
         protected void Awake()
         {
             if (Instance != null && Instance != this)
@@ -37,16 +37,25 @@ namespace Ibralogue
             }
         }
 
+        /// <summary>
+        /// Starts a dialogue by clearing the dialogue box and starting the <see href="DisplayDialogue"/>DisplayDialogue</see> function.
+        /// </summary>
+        /// <param name="interactionDialogue">The initial Dialogue that we want to use in the conversation</param>
         public void StartConversation(TextAsset interactionDialogue)
         {
             _parsedDialogues = DialogueParser.ParseDialogue(interactionDialogue);
             ClearDialogueBox();
             StartCoroutine(DisplayDialogue());
         }
-
+        
+        /// <summary>
+        /// The DisplayDialogue coroutine displays the dialogue character by character in a scrolling manner and sets all other
+        /// relevant values.
+        /// </summary>
         private IEnumerator DisplayDialogue()
         {
             nameText.text = _parsedDialogues[_currentDialogueIndex].speaker;
+            _linePlaying = true;
             sentenceText.text = _parsedDialogues[_currentDialogueIndex].sentences[_currentSentenceIndex];
             DisplaySpeakerImage();
 
@@ -55,11 +64,17 @@ namespace Ibralogue
                 sentenceText.maxVisibleCharacters++;
                 yield return new WaitForSeconds(0.1f); 
             }
+            _linePlaying = false;
             yield return null;
         }
-
+        
+        /// <summary>
+        /// Clears the dialogue box and displays the next <see cref="Dialogue"/> if no sentences are left in the
+        /// current one.
+        /// </summary>
         public void DisplayNextLine()
         {
+            if (_linePlaying) return;
             ClearDialogueBox();
             if (_currentSentenceIndex < _parsedDialogues[_currentDialogueIndex].sentences.Count - 1)
             {
@@ -75,17 +90,25 @@ namespace Ibralogue
             }
         }
         
+        /// <summary>
+        /// Sets the speaker image and makes the Image transparent if there is no speaker image.
+        /// </summary>
         private void DisplaySpeakerImage()
         {
             speakerPortrait.color = _parsedDialogues[_currentDialogueIndex].speakerImage == null ? new Color(0,0,0, 0) : new Color(255,255,255,255);
             speakerPortrait.sprite = _parsedDialogues[_currentDialogueIndex].speakerImage;
         }
 
+        /// <summary>
+        /// Clears all text and Images in the dialogue box.
+        /// </summary>
         private void ClearDialogueBox()
         {
             nameText.text = string.Empty;
             sentenceText.text = string.Empty;
+            speakerPortrait.color = new Color(0, 0, 0, 0);
             sentenceText.maxVisibleCharacters = 0;
+            _linePlaying = false;
         }
     }
 }
