73a206bc3be28a200bdb3abbd5ca2497fda84113
Ibrahim Hisham <muhammadibrahimhisham@gmail.com>
2022-10-17 22:51:20 +0500

    add proper logging

---
+++ Ibralogue/Scripts/Core/Parser/DialogueLogger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DialogueLogger : MonoBehaviour
+{
+    public static void LogError(int line, string message)
+    {
+        Report(line,"", message);
+    }
+
+    private static void Report(int line, string where, string message)
+    {
+        Debug.LogError($"[Ibralogue] [line {line}], Error{where}: {message}");
+    }
+}
