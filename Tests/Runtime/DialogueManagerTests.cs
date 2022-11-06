c80e23db92737e815fd87fc3b09646bea409df34
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2022-11-06 17:24:04 +0100

    Add test version of the manager to access protected fields

---
+++ Tests/Runtime/DialogueManagerTests.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ibralogue.Tests
+{
+    public class DialogueManagerTests : MonoBehaviour
+    {
+
+    }
+}
