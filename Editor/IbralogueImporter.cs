aba9d2772deeb6143a19f9f210bac3f99956e22c
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2022-11-06 15:00:49 +0100

    add support for new API for ease of developement

--- Editor/IbralogueImporter.cs
+++ Editor/IbralogueImporter.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using System.IO;
+
+#if UNITY_2020_2_OR_NEWER
+using UnityEditor.AssetImporters;
+#else
 using UnityEditor.Experimental.AssetImporters;
+#endif
 
 namespace Ibralogue.Editor
 {
