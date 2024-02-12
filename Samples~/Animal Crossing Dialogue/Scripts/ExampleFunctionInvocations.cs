b319b8b97bb22804df1edc8359fcdba1d39d0097
Ibrahim Hisham <muhammadibrahimhisham@gmail.com>
2024-02-12 17:55:37 +0500

    reorder logs for clarity

--- Samples~/Animal Crossing Dialogue/Scripts/ExampleFunctionInvocations.cs
+++ Samples~/Animal Crossing Dialogue/Scripts/ExampleFunctionInvocations.cs
@@ -8,8 +8,8 @@ public static class ExampleFunctionInvocations
     [DialogueFunction]
     public static void TriggerFunction(DialogueManagerBase<Button> dialogueManager)
     {
-        Debug.Log("Function Trigger");
         Debug.Log($"There are {dialogueManager.ParsedConversations.Count} conversations in this file.");
+        Debug.Log("Function Trigger");
     }
         
     [DialogueFunction]
