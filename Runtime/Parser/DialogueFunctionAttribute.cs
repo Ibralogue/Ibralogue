b351f9d6ef990dc1b6c6d4cce9d0c71875f35fc2
ibrahim hisham <muhammadibrahimhisham@gmail.com>
2026-03-19 02:59:33 +0500

    remove deprecated DialogueFunctionAttribute legacy alias

--- Runtime/Parser/DialogueFunctionAttribute.cs
+++ Runtime/Parser/DialogueFunctionAttribute.cs
@@ -6,10 +6,4 @@ namespace Ibralogue
 	/// Marks a static method as a custom invocation that can be called from dialogue files.
 	/// </summary>
 	public class DialogueInvocationAttribute : Attribute {}
-
-	/// <summary>
-	/// Legacy alias for <see cref="DialogueInvocationAttribute"/>.
-	/// </summary>
-	[Obsolete("Use [DialogueInvocation] instead.")]
-	public class DialogueFunctionAttribute : DialogueInvocationAttribute {}
 }
