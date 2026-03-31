834c643c12a75ce2edb77199949e7091c230ece2
ibrahim hisham <muhammadibrahimhisham@gmail.com>
2026-03-31 11:00:24 +0500

    add InternalsVisibleTo for runtime test assembly

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -3,3 +3,4 @@
 [assembly: InternalsVisibleTo("Ibralogue.Editor")]
 [assembly: InternalsVisibleTo("Ibralogue.Editor.Tests")]
 [assembly: InternalsVisibleTo("Ibralogue.Tests")]
+[assembly: InternalsVisibleTo("Ibralogue.Runtime.Tests")]
